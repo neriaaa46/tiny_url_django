fe12b57477723a0bd676a6e0777639a2fa125984
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-11-29 23:32:20 +0200

    added tests

--- tiny_url/api/tests.py
+++ tiny_url/api/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from api.models import TinyUrl
 
-# Create your tests here.
+
+class ApiTest(TestCase):
+
+    def setUp(self):
+        TinyUrl.objects.create(id="123", url="http://google.com", counter=0)
+
+    def test_redirect(self):
+        response = self.client.get('/s/123')
+        self.assertEqual(302, response.status_code)
+        self.assertEqual("http://google.com", response.url)
+
+    def test_tiny_url_not_found(self):
+        response = self.client.get('/s/NOT_EXIST')
+        self.assertEqual(404, response.status_code)
+
+    def test_create_tiny_url_empty_body(self):
+        response = self.client.post('/create', {}, content_type="application/json")
+        self.assertEqual(400, response.status_code)
+
+    # def test_create_invalid_tiny_url(self):
+    #     response = self.client.post('/create', {"url": "123"}, content_type="application/json")
+    #     self.assertEqual(400, response.status_code)
+
+    def test_create_tiny_url(self):
+        response = self.client.post('/create', {"url": "http://amazon.com"}, content_type="application/json")
+        self.assertEqual(200, response.status_code)
+        url = response.content.decode("UTF-8")
+        tiny_id = self.extract_id_from_url(url)
+        tiny_url = TinyUrl.objects.get(id=tiny_id)
+        self.assertEqual("http://amazon.com", tiny_url.url)
+
+    def extract_id_from_url(self, url):
+        return url.rpartition('/')[-1]
