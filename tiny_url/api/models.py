8b902e229eb94ee0a29c1e7ca1713211649cb3d7
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-11-29 23:31:16 +0200

    updated model

--- tiny_url/api/models.py
+++ tiny_url/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TinyUrl(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    url = models.CharField(max_length=1024)
+    counter = models.BigIntegerField(default=0)
+
